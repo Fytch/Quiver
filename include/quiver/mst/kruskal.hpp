446d31c66e0a4021a10c7fd8b8ad6538b91b3af0
Fytch <josua.rieder1996@gmail.com>
2018-06-07 14:57:27 +0200

    implement Kruskal's algorithm for weighted graphs

---
+++ include/quiver/mst/kruskal.hpp
@@ -0,0 +1,73 @@
+/*
+ *	Quiver - A graph theory library
+ *	Copyright (C) 2018 Josua Rieder (josua.rieder1996@gmail.com)
+ *	Distributed under the MIT License.
+ *	See the enclosed file LICENSE.txt for further information.
+ */
+
+#ifndef QUIVER_MST_KRUSKAL_HPP_INCLUDED
+#define QUIVER_MST_KRUSKAL_HPP_INCLUDED
+
+// #include <quiver/adjacency_list.hpp>
+#include <quiver/disjoint_set.hpp>
+#include <vector>
+#include <algorithm>
+#include <type_traits>
+
+namespace quiver
+{
+	template<typename graph_t>
+	std::enable_if_t<is_weighted_v<graph_t>, graph_t> kruskal(graph_t const& graph)
+	{
+		static_assert(is_undirected_v<graph_t>, "kruskal opereaters on undirected graphs");
+
+		disjoint_set<> cc(graph.V());
+		struct edge_t
+		{
+			vertex_index_t from, to;
+			typename graph_t::out_edge_t const* ptr;
+
+			constexpr edge_t(decltype(from) from, decltype(to) to, decltype(ptr) ptr)
+			: from(std::move(from)), to(std::move(to)), ptr(std::move(ptr))
+			{
+			}
+		};
+		std::vector<edge_t> edges;
+		{
+			const std::size_t E = graph.E();
+			edges.reserve(E);
+			const auto v_end = graph.v_end();
+			std::size_t v_index = 0;
+			for(auto vert = graph.v_begin(); vert != v_end; ++vert, ++v_index) {
+				auto const& out_edges = vert->out_edges;
+				const auto e_end = out_edges.end();
+				for(auto edge = out_edges.begin(); edge != e_end; ++edge)
+					if(v_index < edge->to) // we only need half of the actual edges
+						edges.emplace_back(v_index, edge->to, &*edge);
+			}
+		}
+		std::sort(edges.begin(), edges.end(), [](edge_t const& lhs, edge_t const& rhs){ return lhs.ptr->weight < rhs.ptr->weight; });
+
+		graph_t mst = graph.vertices();
+		for(edge_t const& e : edges)
+		{
+			if(!cc.same_set(e.from, e.to))
+			{
+				mst.add_edge(e.from, e.to, *e.ptr->properties());
+				cc.unite(e.from, e.to);
+			}
+		}
+		return mst;
+	}
+	/*
+	template<typename graph_t>
+	std::enable_if_t<!is_weighted_v<graph_t>, graph_t> kruskal(graph_t const& graph)
+	{
+		static_assert(is_undirected_v<graph_t>, "");
+
+		// TODO:
+	}
+	*/
+}
+
+#endif // !QUIVER_MST_KRUSKAL_HPP_INCLUDED
---
+++ test/quiver/mst/kruskal.cpp
@@ -0,0 +1,83 @@
+/*
+ *	Quiver - A graph theory library
+ *	Copyright (C) 2018 Josua Rieder (josua.rieder1996@gmail.com)
+ *	Distributed under the MIT License.
+ *	See the enclosed file LICENSE.txt for further information.
+ */
+
+#include <catch.hpp>
+#include <quiver.hpp>
+using namespace quiver;
+
+TEST_CASE("kruskal", "[quiver][mst]")
+{
+	SECTION("weighted")
+	{
+		using graph_t = adjacency_list<undirected, wt<int>, void>;
+		SECTION("empty")
+		{
+			graph_t empty(0);
+			auto mst = kruskal(empty);
+			CHECK(mst.empty());
+		}
+		SECTION("trivial (0 edges)")
+		{
+			graph_t trivial(1);
+			auto mst = kruskal(trivial);
+			CHECK(mst.V() == 1);
+			CHECK(mst.E() == 0);
+		}
+		SECTION("trivial (1 edge)")
+		{
+			graph_t trivial(1);
+			trivial.add_edge(0, 0, 3);
+			auto mst = kruskal(trivial);
+			CHECK(mst.V() == 1);
+			CHECK(mst.E() == 0);
+		}
+		SECTION("1 cc")
+		{
+			graph_t graph(6);
+			graph.add_edge(0, 1, 1);
+			graph.add_edge(0, 2, 3);
+			graph.add_edge(0, 3, 3);
+			graph.add_edge(1, 2, 2);
+			graph.add_edge(2, 3, 4);
+			graph.add_edge(2, 5, 7);
+			graph.add_edge(3, 4, 1);
+			graph.add_edge(4, 5, 9);
+
+			auto mst = kruskal(graph);
+			REQUIRE(mst.V() == graph.V());
+			REQUIRE(mst.E() == graph.V() - 1); // because we have 1 CC
+			CHECK(mst.get_edge(0, 1) != nullptr);
+			CHECK(mst.get_edge(0, 2) == nullptr);
+			CHECK(mst.get_edge(0, 3) != nullptr);
+			CHECK(mst.get_edge(1, 2) != nullptr);
+			CHECK(mst.get_edge(2, 3) == nullptr);
+			CHECK(mst.get_edge(2, 5) != nullptr);
+			CHECK(mst.get_edge(3, 4) != nullptr);
+			CHECK(mst.get_edge(4, 5) == nullptr);
+		}
+		SECTION("2 cc")
+		{
+			graph_t graph(6);
+			graph.add_edge(0, 1, 1);
+			graph.add_edge(0, 2, 3);
+			graph.add_edge(0, 3, 3);
+			graph.add_edge(1, 2, 2);
+			graph.add_edge(2, 3, 4);
+			graph.add_edge(4, 5, 9);
+
+			auto mst = kruskal(graph);
+			REQUIRE(mst.V() == graph.V());
+			REQUIRE(mst.E() == graph.V() - 2); // because we have 2 CCs
+			CHECK(mst.get_edge(0, 1) != nullptr);
+			CHECK(mst.get_edge(0, 2) == nullptr);
+			CHECK(mst.get_edge(0, 3) != nullptr);
+			CHECK(mst.get_edge(1, 2) != nullptr);
+			CHECK(mst.get_edge(2, 3) == nullptr);
+			CHECK(mst.get_edge(4, 5) != nullptr);
+		}
+	}
+}
