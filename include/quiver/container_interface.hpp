bbace8845f0dca521409cf248db58386e2479aab
Fytch <josua.rieder1996@gmail.com>
2018-06-26 11:43:26 +0200

    add container interface for sorting

--- include/quiver/container_interface.hpp
+++ include/quiver/container_interface.hpp
@@ -10,6 +10,8 @@
 
 #include <vector>
 #include <type_traits>
+#include <iterator>
+#include <algorithm>
 
 namespace quiver
 {
@@ -23,7 +25,30 @@ namespace quiver
 		std::is_nothrow_move_assignable_v<T> &&
 		std::is_nothrow_swappable_v<T>;
 
+	namespace detail
+	{
+		template<typename container_t>
+		struct is_ordered_helper
+		{
+		};
+		template<typename T>
+		struct is_ordered_helper<vector<T>> : std::true_type
+		{
+		};
+	}
+	template<typename container_t>
+	inline constexpr bool is_ordered = detail::is_ordered_helper<container_t>::value;
+	template<template<typename> typename container>
+	inline constexpr bool is_ordered_templ = is_ordered<container<int>>;
+
 	// TODO: uniform interface for different container types
+
+	template<typename container_t, typename comparator_t>
+	void sort(container_t& container, comparator_t comparator)
+	{
+		using std::begin, std::end;
+		std::sort(begin(container), end(container), comparator);
+	}
 }
 
 #endif // !QUIVER_CONTAINER_INTERFACE_HPP_INCLUDED
