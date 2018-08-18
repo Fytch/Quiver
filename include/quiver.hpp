5cfc7bc15a9c3294814f2e35636e61038ba0b022
Fytch <josua.rieder1996@gmail.com>
2018-08-18 19:40:52 +0200

    add [heaps] submodule and implement binary heaps

--- include/quiver.hpp
+++ include/quiver.hpp
@@ -14,6 +14,7 @@
 #include <quiver/disjoint_set.hpp>
 #include <quiver/exception.hpp>
 #include <quiver/families.hpp>
+#include <quiver/heaps.hpp>
 #include <quiver/mst.hpp>
 #include <quiver/operations.hpp>
 #include <quiver/orientation.hpp>
