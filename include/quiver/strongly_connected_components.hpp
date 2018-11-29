2522bfc07ea9920f63f6d7444835ae570bfbce04
Pascal Sommer <p@pascalsommer.ch>
2018-11-29 09:59:05 +0100

    implement tarjan_recursive, tarjan_recursive tests

--- include/quiver/strongly_connected_components.hpp
+++ include/quiver/strongly_connected_components.hpp
@@ -9,27 +9,72 @@
 #define QUIVER_STRONGLY_CONNECTED_COMPONENTS_HPP_INCLUDED
 
 #include <quiver/adjacency_list.hpp>
-#include <quiver/disjoint_set.hpp>
 #include <cstddef>
+#include <stack>
 #include <vector>
-#include <unordered_map>
 
 namespace quiver
 {
-	template<typename graph_t>
-	disjoint_set<> get_disjoint_set(graph_t const& graph)
+	namespace tarjan_detail
 	{
-		static_assert(is_undirected_v<graph_t>, "connected components exist in undirected graphs");
+		struct tarjan_state{
+            std::vector<bool> discovered;
+            std::vector<std::size_t> scc_index; // -1 means not set
+            std::vector<vertex_index_t> lowlink;
+            std::stack<std::size_t> scc_stack;
+            std::vector<bool> on_scc_stack;
+			std::size_t current_scc_index {0};
 
-		disjoint_set<> cc(graph.V());
-		vertex_index_t vert_index = 0;
-		for(auto const& vert : graph) {
-			for(auto const& out_edge : vert.out_edges)
-				if(vert_index < out_edge.to) // we only need half of the actual edges
-					cc.unite(vert_index, out_edge.to);
-			++vert_index;
+            std::size_t nr_sccs_found {0};
+            std::vector<std::size_t> node_to_scc_index_mapping;
+
+			tarjan_state(std::size_t V)
+                : discovered(V, false),
+				  scc_index(V),
+				  lowlink(V),
+				  on_scc_stack(V, false),
+                  node_to_scc_index_mapping(V)
+			{}
+		};
+
+		template<
+			typename edge_properties_t,
+			typename vertex_properties_t,
+			template<typename> class out_edge_container,
+			template<typename> class vertex_container
+		>
+		void tarjan_recurse(const vertex_index_t v,
+						    adjacency_list<directed, edge_properties_t, vertex_properties_t, out_edge_container, vertex_container> const& graph,
+							tarjan_state& state)
+		{
+			state.scc_index[v] = state.current_scc_index;
+            state.discovered[v] = true;
+			state.lowlink[v] = state.current_scc_index;
+			++state.current_scc_index;
+
+			state.scc_stack.push(v);
+			state.on_scc_stack[v] = true;
+
+			for(const auto& out_edge : graph.vertex(v).out_edges) {
+                vertex_index_t w = out_edge.to;
+				if(!state.discovered[w]) {
+                    tarjan_recurse(w, graph, state);
+                    state.lowlink[v] = std::min(state.lowlink[v], state.lowlink[w]);
+				} else if (state.on_scc_stack[w]) {
+                    state.lowlink[v] = std::min(state.lowlink[v], state.scc_index[w]);
+                }
+			}
+
+            if (state.lowlink[v] == state.scc_index[v]) {
+                vertex_index_t w;
+                do {
+                    w = state.scc_stack.top(); state.scc_stack.pop();
+                    state.on_scc_stack[w] = false;
+                    state.node_to_scc_index_mapping[w] = state.nr_sccs_found;
+                } while (w != v);
+                ++ state.nr_sccs_found;
+            }
 		}
-		return cc;
 	}
 
 	template<
@@ -38,39 +83,17 @@ namespace quiver
 		template<typename> class out_edge_container,
 		template<typename> class vertex_container
 	>
-	std::vector<adjacency_list<directed, edge_properties_t, vertex_properties_t, out_edge_container, vertex_container>>
-	split_ccs(adjacency_list<directed, edge_properties_t, vertex_properties_t, out_edge_container, vertex_container> const& graph)
+	std::vector<std::size_t> // vertex [0..V] -> scc index
+	tarjan_recursive(adjacency_list<directed, edge_properties_t, vertex_properties_t, out_edge_container, vertex_container> const& graph)
 	{
-		using graph_t = adjacency_list<directed, edge_properties_t, vertex_properties_t, out_edge_container, vertex_container>;
-		auto ds = get_disjoint_set(graph);									// vertex [0..V] -> root [0..V]
+		tarjan_detail::tarjan_state state(graph.V());
 
-		std::unordered_map<std::size_t, std::size_t> compressed_cc_index;	// disjoint set roots -> cc index [0..|CC|]
-		for(vertex_index_t v = 0; v < graph.V(); ++v)
-			compressed_cc_index.try_emplace(ds.find(v), compressed_cc_index.size());
-
-		std::vector<std::size_t> cc_relative(graph.V());					// vertex [0..V] -> index in cc [0..V-|CC|+1]
-		{
-			std::vector<std::size_t> counters(ds.sets(), 0);
-			for(vertex_index_t v = 0; v < graph.V(); ++v)
-				cc_relative[v] = counters[compressed_cc_index[ds.find(v)]]++;
+		for(vertex_index_t start_node = 0; start_node < graph.V(); ++start_node) {
+			if(!state.discovered[start_node])
+				tarjan_recurse(start_node, graph, state);
 		}
 
-		std::vector<graph_t> result(ds.sets());
-		for(vertex_index_t v = 0; v < graph.V(); ++v) {
-			auto& cc = result[compressed_cc_index[ds.find(v)]];
-			cc.reserve(ds.cardinality(v));
-			auto index = cc.add_vertex(std::move(graph.vertex(v)));
-			for(auto& out_edge : cc.vertex(index).out_edges)
-				out_edge.to = cc_relative[out_edge.to];
-		}
-		return result;
-	}
-
-	template<typename graph_t>
-	std::size_t ccs(graph_t const& graph)
-	{
-		static_assert(is_undirected_v<graph_t>, "connected components exist in undirected graphs");
-		return get_disjoint_set(graph).sets();
+		return state.node_to_scc_index_mapping;
 	}
 }
 
---
+++ test/quiver/strongly_connected_components.cpp
@@ -0,0 +1,69 @@
+/*
+ *	Quiver - A graph theory library
+ *	Copyright (C) 2018 Josua Rieder (josua.rieder1996@gmail.com)
+ *	Distributed under the MIT License.
+ *	See the enclosed file LICENSE.txt for further information.
+ */
+
+#include <catch.hpp>
+#include <quiver.hpp>
+using namespace quiver;
+
+TEST_CASE("strongly_connected_components", "[quiver][fundamentals]")
+{
+	using graph_t = adjacency_list<directed, void, cap<int>>;
+	graph_t graph;
+	const std::size_t N = 8;
+	for(std::size_t i = 0; i < N; ++i)
+		graph.add_vertex(i);
+
+
+	// graph from example on Wikipedia
+	// https://en.wikipedia.org/wiki/Tarjan's_strongly_connected_components_algorithm
+
+	// numbering:
+	// 0  2  4  6
+	// 1  3  5  7
+
+	graph.add_edge(0, 1);
+	graph.add_edge(1, 2);
+	graph.add_edge(2, 0);
+
+	graph.add_edge(3, 1);
+	graph.add_edge(3, 2);
+	graph.add_edge(3, 5);
+	graph.add_edge(5, 3);
+	graph.add_edge(5, 4);
+
+	graph.add_edge(4, 2);
+	graph.add_edge(4, 6);
+	graph.add_edge(6, 4);
+
+	graph.add_edge(7, 6);
+	graph.add_edge(7, 5);
+
+	const auto sccs = tarjan_recursive(graph);
+
+	REQUIRE(sccs.size() == N);
+
+	CHECK(sccs[0] == sccs[1]);
+	CHECK(sccs[0] == sccs[2]);
+
+	CHECK(sccs[3] == sccs[5]);
+
+	CHECK(sccs[4] == sccs[6]);
+
+	CHECK(sccs[0] != sccs[3]);
+	CHECK(sccs[0] != sccs[4]);
+	CHECK(sccs[0] != sccs[7]);
+
+	CHECK(sccs[3] != sccs[4]);
+	CHECK(sccs[3] != sccs[7]);
+
+	CHECK(sccs[4] != sccs[7]);
+
+	std::size_t max_scc_index = *std::max_element(sccs.cbegin(), sccs.cend());
+
+	// 4 components
+	CHECK(max_scc_index == 3);
+}
