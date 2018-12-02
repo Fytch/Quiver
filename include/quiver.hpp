a63769530897f10301e274cc05abab2c4265f807
Pascal Sommer <p@pascalsommer.ch>
2018-12-02 21:36:23 +0100

    update license

--- include/quiver.hpp
+++ include/quiver.hpp
@@ -1,6 +1,7 @@
 /*
  *	Quiver - A graph theory library
  *	Copyright (C) 2018 Josua Rieder (josua.rieder1996@gmail.com)
+ *                     Pascal Sommer (p@pascalsommer.ch)
  *	Distributed under the MIT License.
  *	See the enclosed file LICENSE.txt for further information.
  */
